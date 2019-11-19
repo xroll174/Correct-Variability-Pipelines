d2a3ff7dc552ee410420c9cbbeed21a27f5662eb
xavier rolland <xavier.rolland@inria.fr>
2019-11-19 13:42:39 +0100

    comments in false_positive_rate and full_analysis

--- src/false_positive_rate.m
+++ src/false_positive_rate.m
@@ -8,10 +8,11 @@ function [fract,n1,n2] = false_positive_rate(smooth1,smooth2,reg1,reg2,der1,der2
     der2 = char(string(der2));
     dossier = char(string(dossier));
 
-    V1 = spm_read_vols(spm_vol([data_path,'/',dossier,'/smooth_',smooth1,'_',smooth2,'/reg_',reg1,'_',reg2,'/der_',der1,'_',der2,'/spmT_0001.nii']));
-    V2 = spm_read_vols(spm_vol([data_path,'/',dossier,'/smooth_',smooth1,'_',smooth2,'/reg_',reg1,'_',reg2,'/der_',der1,'_',der2,'/mask.nii']));
+    V1 = spm_read_vols(spm_vol(fullfile(data_path,'data',dossier,['smooth_',smooth1,'_',smooth2],['reg_',reg1,'_',reg2],['der_',der1,'_',der2],'spmT_0001.nii')));
+    V2 = spm_read_vols(spm_vol(fullfile(data_path,'data',dossier,['smooth_',smooth1,'_',smooth2],['reg_',reg1,'_',reg2],['der_',der1,'_',der2],'mask.nii')));
     n1 = sum(V1(:) > 1.6716)
-    % S'assurer que la stat correspond bien à df = 58
+
+    
     n2 = sum(V2(:) ~= 0)
     fract = n1/n2
 end
--- src/full_analysis.m
+++ src/full_analysis.m
@@ -1,6 +1,18 @@
 function [rate] = full_analysis(list_1,list_2,smooth1,smooth2,reg1,reg2,der1,der2,dossier)
+
+    % for the same input as the second_level_analysis function (couple of
+    % groups of subjects and pipeline parameter values, and folder name),
+    % we perform preprocessing and first-level analysis for the given
+    % parameters on every subject if it has not already been done, and then
+    % the second-level analysis between both groups. The function returns
+    % the proportion of false positives in the resulting statistical map
+    
     A = [list_1,list_2]
+    
     FLA_list(A)
+    
     second_level_analysis(list_1,list_2,smooth1,smooth2,reg1,reg2,der1,der2,dossier)
+    
     rate = false_positive_rate(smooth1,smooth2,reg1,reg2,der1,der2,dossier)
+    % (to be modified)
 end
