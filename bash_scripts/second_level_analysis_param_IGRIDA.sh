c39700a602b88e358de6097bf237a9874fa608fb
xavier rolland <xavier.rolland@inria.fr>
2020-04-07 13:58:14 +0200

    modification of second-level analysis files to add sample size as a parameter

--- bash_scripts/second_level_analysis_param_IGRIDA.sh
+++ bash_scripts/second_level_analysis_param_IGRIDA.sh
@@ -4,10 +4,11 @@ reg1=$3
 reg2=$4
 der1=$5
 der2=$6
+ng=$7
 
 DIR=$( dirname ${BASH_SOURCE})
 
 for ((i=0; i<100;i++))
 do
-    oarsub -l /nodes=1/core=4,walltime=1:20:0 -p "dedicated='neurinfo'" ". /etc/profile.d/modules.sh; module load octave-5.1.0-gcc-9.1.0; $DIR/second_level_analysis_param.sh $smooth1 $smooth2 $reg1 $reg2 $der1 $der2 $i"
+    oarsub -l /nodes=1/core=4,walltime=1:20:0 -p "dedicated='neurinfo'" ". /etc/profile.d/modules.sh; module load octave-5.1.0-gcc-9.1.0; $DIR/second_level_analysis_param.sh $smooth1 $smooth2 $reg1 $reg2 $der1 $der2 $i $ng"
 done
--- src/second_level_analysis_octave_mult.m
+++ src/second_level_analysis_octave_mult.m
@@ -1,6 +1,11 @@
-function [] = second_level_analysis_octave_mult(smooth1,smooth2,reg1,reg2,der1,der2,b1,b2)
-    G50 = importdata('list_couples_subjects_HCP_50.mat');
-    for i=b1:b2
-        second_level_analysis_octave(G50(i,1:50),G50(i,51:100),smooth1,smooth2,reg1,reg2,der1,der2,['SLA',num2str(i),'_50'])
+function [] = second_level_analysis_octave_mult(smooth1,smooth2,reg1,reg2,der1,der2,b1,b2,ng)
+    file_name=['list_couples_subjects_HCP_',num2str(ng),'.mat']
+    if ~isfile(filename)
+        error('Error: list of groups id does not exist')
+    else
+        G = importdata(filename);
+        for i=b1:b2
+            second_level_analysis_octave(G(i,1:ng),G50(i,(ng+1):(2*ng)),smooth1,smooth2,reg1,reg2,der1,der2,['SLA',num2str(i),'_',num2str(ng)])
+        end
     end
 end
