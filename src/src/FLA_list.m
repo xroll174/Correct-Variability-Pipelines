cbc95bd09f25180ad086c69afc9cbbe550ec715a
xavier rolland <xavier.rolland@inria.fr>
2020-04-14 13:40:24 +0200

    removing useless files

--- src/src/FLA_list.m
+++
@@ -1,9 +0,0 @@
-function [] = FLA_list(list_subjects,smooth,reg,der)
-
-    % We perform first level analysis for multiple subjects and for
-    % specified parameter values for smoothing, number of regressors and
-    % temporal derivatives
-    
-    parfor i=1:length(list_subjects)
-        first_level_analysis(list_subjects(i),smooth,reg,der)
-end
